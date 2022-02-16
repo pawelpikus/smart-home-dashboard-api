ab92356f0af11f0b87fb31f3be74e72065a0dea5
godspeed7 <pawelpikus7@gmail.com>
2022-02-16 19:52:23 +0100

    adding cors origin options

--- index.js
+++ index.js
@@ -16,10 +16,7 @@ app.get("/", (req, res) => {
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: [
-      "http://localhost:3000",
-      "https://smart-home-dashboard-api.herokuapp.com/",
-    ],
+    origin: ["http://localhost:3000", /\.herokuapp\.com$/],
   },
 });
 
