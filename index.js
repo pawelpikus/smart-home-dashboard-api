dcabbd373856f2697f360b4d07a866ae27a3bc90
godspeed7 <pawelpikus7@gmail.com>
2022-02-17 19:22:16 +0100

    udpdating server logic to change data every 2s

--- index.js
+++ index.js
@@ -7,7 +7,33 @@ const path = require("path");
 const port = process.env.PORT || 4001;
 const app = express();
 const pathToFile = path.resolve("./feed.json");
-const getDeviceDetails = () => JSON.parse(fs.readFileSync(pathToFile));
+
+const updateFeed = () => {
+  const { SmartBulb, SmartOutlet, SmartTemperatureSensor } = JSON.parse(
+    fs.readFileSync(pathToFile)
+  );
+
+  SmartOutlet.powerConsumption === 6
+    ? (SmartOutlet.powerConsumption = 10)
+    : (SmartOutlet.powerConsumption = 6);
+
+  SmartBulb.brightness === 78
+    ? (SmartBulb.brightness = 85)
+    : (SmartBulb.brightness = 78);
+
+  SmartTemperatureSensor.temperature === 21
+    ? (SmartTemperatureSensor.temperature = 23)
+    : (SmartTemperatureSensor.temperature = 21);
+
+  fs.writeFileSync(
+    pathToFile,
+    JSON.stringify({ SmartBulb, SmartOutlet, SmartTemperatureSensor }, null, 2)
+  );
+};
+
+setInterval(() => {
+  updateFeed();
+}, 2000);
 
 app.get("/", (req, res) => {
   res.status(200).send("Server ready");
@@ -22,18 +48,24 @@ const io = new Server(httpServer, {
     ],
   },
 });
+const getDeviceDetails = () => JSON.parse(fs.readFileSync(pathToFile));
 
-const smartDeviceDetails = getDeviceDetails();
+let interval;
 
 io.on("connection", (socket) => {
   console.log(`New client connected,${socket.id} `);
-
-  setTimeout(() => getApiAndEmit(socket), 3000);
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 2000);
   socket.on("disconnect", () => {
+    clearInterval(interval);
     console.log(`Client disconnected, ${socket.id}`);
   });
 });
 const getApiAndEmit = (socket) => {
-  socket.emit("refresh", smartDeviceDetails);
+  getDeviceDetails();
+
+  socket.emit("refresh", getDeviceDetails());
 };
 httpServer.listen(port, () => console.log(`Listening on port ${port}`));
